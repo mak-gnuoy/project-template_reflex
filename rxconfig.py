43051ae8f955b744e30d3572e487cf555e7dd09f
mak-gnuoy <mak.gnuoy@gmail.com>
2025-06-06 11:29:20 +0900

    unused directories removed

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,7 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="myapp",
-    app_module_import="app",
+    app_name="app",
+    app_module_import="app.myapp",
     plugins=[rx.plugins.TailwindV3Plugin()],
 )
